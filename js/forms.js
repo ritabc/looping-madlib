aa6ccffc9a0f457eb5ea04bfe2a2dc45a558a12d
cynthia smith and scott fraser <scott@>
2018-05-30 12:03:45 -0700

    set up shout form

---
+++ js/forms.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $("#shout").submit(function(event) {
+    var shoutback = $("input#text").val();
+
+
+    $(".text").text(shoutback);
+
+    $("#holler").show();
+
+    event.preventDefault();
+  });
+});
